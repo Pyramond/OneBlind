d035485ef8e0299373a682c03355a8c46bc78392
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-07 19:41:55 +0200

    Create tournamentPlayer slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import tournamentPlayerReducer from "./slices/tournamentPage/players";
 
 export default configureStore({
     reducer: {
-        
-    }
-})
+        tournamentPlayers: tournamentPlayerReducer,
+    },
+});
