49dabd1ee541ff564de8a0c93ab5b32b46529f34
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-07 23:04:24 +0200

    add info reducer

---
+++ src/redux/slices/tournamentPage/info.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const tournamentInfo = createSlice({
+    name: "tournamentInfoSlice",
+    initialState: {
+        name: "",
+        date: 0,
+        blindName: "",
+        blindId: 0,
+        initialChip: 0
+    },
+    reducers: {
+        setInfos: (state, action) => {
+            state.name = action.payload.name
+            state.date = action.payload.date
+            state.blindName = action.payload.blindName
+            state.blindId = action.payload.blindId
+            state.initialChip = action.payload.initialChip
+        }
+    },
+});
+
+export const { setInfos } = tournamentInfo.actions;
+export default tournamentInfo.reducer;
