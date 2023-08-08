b92500290985b97d0269be3ead3a5c715977953c
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-08 22:22:41 +0200

    Create tournament steps reducer

--- src/redux/slices/tournamentPage/steps.js
+++ src/redux/slices/tournamentPage/steps.js
@@ -4,13 +4,19 @@ export const tournamentSteps = createSlice({
     name: "tournamentStepsSlice",
     initialState: {
         steps: [],
+        currentStep: {},
+        index: 0
     },
     reducers: {
         setSteps: (state, action) => {
             state.steps = action.payload
+        },
+        changeStep: (state) => {
+            state.currentStep = state.steps[state.index]
+            state.index = state.index + 1
         }
     },
 });
 
-export const { setSteps } = tournamentSteps.actions;
+export const { setSteps, changeStep } = tournamentSteps.actions;
 export default tournamentSteps.reducer;
