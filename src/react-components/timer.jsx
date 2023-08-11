8f388dbdf4c48aa21776be47b3d5bbd5710daac1
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-12 01:43:48 +0200

    Fix bug when next step time is the same as current time

--- src/react-components/timer.jsx
+++ src/react-components/timer.jsx
@@ -1,7 +1,7 @@
 import { useTimer } from 'react-timer-hook';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeStep } from '../redux/slices/tournamentPage/steps';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { Button } from 'react-bootstrap';
 
 
@@ -10,6 +10,8 @@ export default function MyTimer() {
     const t = useSelector((state) => state.tournamentSteps);
     const dispatch = useDispatch();
     const [currentTime, setCurrentTime] = useState(0)
+
+    const effectDependency = useMemo(() => ({ currentStep: t.currentStep, random: Math.random() }), [t.currentStep]);
     
     const {
         totalSeconds,
@@ -31,10 +33,10 @@ export default function MyTimer() {
     });
 
     useEffect(() => {
-      const newExpiryTimestamp = new Date(new Date().getTime() + t.currentStep.time * 60000);
-      restart(newExpiryTimestamp);
-      setCurrentTime(t.currentStep.time)
-  }, [t.currentStep.time]);
+        const newExpiryTimestamp = new Date(new Date().getTime() + t.currentStep.time * 60000);
+        restart(newExpiryTimestamp);
+        setCurrentTime(t.currentStep.time)
+  }, [effectDependency]);
 
 
 
@@ -43,14 +45,17 @@ export default function MyTimer() {
             <div style={{fontSize: '180px', color: "white"}}>
                 <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
             </div>
-            <p>{isRunning ? 'Running' : 'Not running'}</p>
-            <p>{t.currentStep.type}</p>
+
+            <p id="info">{isRunning ? 'Running' : 'Not running'}</p>
+            <p id="info">{t.currentStep.type}</p>
+            <p id="info">Étape {t.currentStep.order}</p>
+
             <Button variant='success' onClick={resume} id="timerButtons">Resume</Button>
             <Button variant="danger" onClick={pause} id="timerButtons">Pause</Button>
             <Button variant="secondary" onClick={() => {
                 const time = new Date();
                 time.setSeconds(time.getSeconds() + currentTime);
-                restart(time);
+                restart(time); // Ce restart exécute le OnExpire donc passer à l'étape d'après
             }} id="timerButtons" >Restart</Button>
         </div>
     );
