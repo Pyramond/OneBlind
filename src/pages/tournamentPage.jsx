50cbe6f39432fc25ee04c0d4b7e0c9907e427fd8
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-14 15:19:05 +0200

    Add blindTab component

--- src/pages/tournamentPage.jsx
+++ src/pages/tournamentPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPlayers } from '../redux/slices/tournamentPage/players';
 import { setInfos } from "../redux/slices/tournamentPage/info"
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getTournamentPlayers, getTournamentById } from '../utils/tournaments';
 import { getModelById } from '../utils/models';
 import { setSteps, changeStep } from '../redux/slices/tournamentPage/steps';
@@ -14,13 +14,14 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { TournamentPlayers } from '../react-components/tournamentPlayers';
 import AvStack from '../react-components/avStack';
-
+import BlindTab from '../react-components/blindTab';
 
 export default function TournamentPage() {
 
     const { id } = useParams();
     const dispatch = useDispatch()
     const t = useSelector((state) => state.tournamentPlayers);
+    const [blindTabComponent, setBlindTabComponent] = useState(<p></p>)
 
     useEffect(() => {
       async function fetchData() {
@@ -29,6 +30,7 @@ export default function TournamentPage() {
 
         const infos = await getTournamentById(id);
         infos.number = players.length;
+        setBlindTabComponent(<BlindTab id={infos.blindId}/>)
         dispatch(setInfos(infos));
 
         const steps = await getModelById(infos.blindId);
@@ -49,7 +51,12 @@ export default function TournamentPage() {
         </Row>
         <Row>
           <Col> <TournamentPlayers id={id} /> </Col>
-          <Col> <AvStack /> </Col>
+          <Col>
+            <div>
+              <AvStack />
+              {blindTabComponent}
+            </div>
+          </Col>
         </Row>
       </Container>
 
