aaefa283484fd44f374a3cd0ae729ec9f5004a6f
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-15 23:35:34 +0200

    deleteTournament at the end of tournament

--- src/react-components/tournamentPlayers.jsx
+++ src/react-components/tournamentPlayers.jsx
@@ -1,16 +1,22 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import { removePlayer } from '../redux/slices/tournamentPage/players';
 import { updateAvStack } from '../redux/slices/tournamentPage/info';
+import { deleteTournament } from "../utils/tournaments"
+import { useNavigate } from 'react-router-dom';
 
 
 export function TournamentPlayers(props) {
 
     const t = useSelector((state) => state.tournamentPlayers);
     const dispatch = useDispatch()
-    
+    const navigate = useNavigate();
     const [isWinner, setIsWinner] = useState(false)
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
         dispatch(updateAvStack(Object.keys(t.value).length))
@@ -45,9 +51,20 @@ export function TournamentPlayers(props) {
         <>
             <div id="tournamentPlayersContainer">
                 {t.value.map((player, index) => (
-                    <p id="player" key={index}>{player.name} {isWinner ? "Gagnant" : <Button variant='danger' onClick={() => { eliminatePlayer(player.id, true) }}>Eliminer</Button>} </p>
+                    <p id="player" key={index}>{player.name} {isWinner ? <div> <p>Gagnant</p> <Button variant="primary" onClick={handleShow}>Terminer le tournois</Button> </div> : <Button variant='danger' onClick={() => { eliminatePlayer(player.id, true) }}>Eliminer</Button>} </p>
                 ))}
             </div>
+
+            <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Terminer ce tournois</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Tu es sur le point de terminer ce tournoi, il ne sera donc plus visible dans la liste des tournois actuels. <br/>Il restera cependant dans l'historique des tournois.</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>Annuler</Button>
+                        <Button variant="danger" onClick={() => { handleClose() ; deleteTournament(props.id) ; navigate("/") }}>Supprimer</Button>
+                    </Modal.Footer>
+            </Modal>
         </>
     )
 }
