b76cc35abe6c78e15314e426ebbd570462a6f50e
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-15 23:07:14 +0200

    Change button text to icons

--- src/react-components/timer.jsx
+++ src/react-components/timer.jsx
@@ -56,16 +56,16 @@ export default function MyTimer() {
             <p id="info">{isFinish ? "Partie Terminée" : t.currentStep.type}</p>
             <p id="info">{isFinish ? `${Object.keys(t.steps).length} / ${Object.keys(t.steps).length}` : `${t.currentStep.order} / ${Object.keys(t.steps).length}`}</p>
 
-            {isPlay ? <Button variant="danger" onClick={() => { pause() ; setIsPlay(false)}} id="timerButtons">Pause</Button> : <Button variant='success' onClick={() => { resume() ; setIsPlay(true)}} id="timerButtons">Resume</Button>}
+            {isPlay ? <Button variant="danger" onClick={() => { pause() ; setIsPlay(false)}} id="timerButtons"> <img src="/images/timer-icons/Pause.svg" alt="Pause_button_image" /> </Button> : <Button variant='success' onClick={() => { resume() ; setIsPlay(true)}} id="timerButtons"> <img src="/images/timer-icons/Play.svg" alt="Play_button_image" /> </Button>}
 
             <Button variant="secondary" onClick={() => {
                 const time = new Date(new Date().getTime() + t.currentStep.time * 60000);
                 restart(time);
-            }} id="timerButtons" >Restart</Button>
+            }} id="timerButtons" > <img src="/images/timer-icons/restart.svg" alt="Restart_button_image" id="Restart_button_image" /> </Button>
 
             <Button variant="secondary" onClick={() => {
                 dispatch(changeStep())
-            }}>Next</Button>
+            }}> <img src="/images/timer-icons/NextArrowForward.svg" alt="Next_button_image" /> </Button>
         </div>
     );
 }
