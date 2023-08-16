3f38bc91d29156cc8a0b352944baeb52816c9409
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-16 22:31:32 +0200

    Add sound when 2 players left

--- src/react-components/tournamentPlayers.jsx
+++ src/react-components/tournamentPlayers.jsx
@@ -9,22 +9,44 @@ import { useNavigate } from 'react-router-dom';
 
 export function TournamentPlayers(props) {
 
+    // Redux
     const t = useSelector((state) => state.tournamentPlayers);
     const dispatch = useDispatch()
     const navigate = useNavigate();
-    const [isWinner, setIsWinner] = useState(false)
-    const [show, setShow] = useState(false);
 
+    const tournamentInfo = useSelector((state) => state.tournamentInfo);
+
+
+    const [isWinner, setIsWinner] = useState(false)
+    
+
+    // Ending modal
+    const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+
+    // Final Modal
+    const [showFinal, setShowFinal] = useState(false)
+    const handleShowFinal = () => setShowFinal(true)
+    const handleCloseFinal = () => setShowFinal(false)
+
+
     useEffect(() => {
         dispatch(updateAvStack(Object.keys(t.value).length))
-
-        if(Object.keys(t.value).length == 1) {
-            setIsWinner(true)
-            eliminatePlayer(t.value[0].id, false)
+        
+        switch (Object.keys(t.value).length) {
+            case 1:
+                setIsWinner(true)
+                eliminatePlayer(t.value[0].id, false)
+                break;
+            case 2:
+                new Audio("/sounds/overtaken.mp3").play()
+                handleShowFinal()
+                break;
         }
+
+
     }, [Object.keys(t.value).length])
 
     function eliminatePlayer(id, remove) {
@@ -64,6 +86,23 @@ export function TournamentPlayers(props) {
                         <Button variant="secondary" onClick={handleClose}>Annuler</Button>
                         <Button variant="danger" onClick={() => { handleClose() ; deleteTournament(props.id) ; navigate("/") }}>Supprimer</Button>
                     </Modal.Footer>
+            </Modal>
+
+            <Modal show={showFinal} onHide={handleCloseFinal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Il ne reste plus que deux joueurs</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p id="modalText">Les deux joueurs restant sont:</p>
+                        <ul>
+                        {t.value.map((player, index) => (
+                            <li id="modalText" key={index}>{player.name}</li>
+                        ))}
+                        </ul>
+                        <p id="modalText">Le tapis moyen est de {tournamentInfo.avStack} avec un total de {tournamentInfo.totalChips} Jetons</p>
+                        <img src="/images/fight.gif" alt="finalGif" id="finalGif"/>
+
+                    </Modal.Body>
             </Modal>
         </>
     )
