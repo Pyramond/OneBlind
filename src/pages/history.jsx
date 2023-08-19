4d02feb86a4e295042fa61f486790cd537c96ceb
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-19 20:47:29 +0200

    Add history table

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -1,11 +1,49 @@
 import NavigationBar from '../react-components/navbar'
+import { useEffect, useState } from 'react'
+import { Table } from 'react-bootstrap'
+import { getAllTournaments } from '../utils/tournaments'
+import { convertTimeStamp } from '../utils/date'
 
 export default function history() {
+
+    const [allTournaments, setAllTournaments] = useState([])
+
+    useEffect(() => {
+        async function fetchData(){
+            const tournaments = await getAllTournaments()
+            tournaments.sort((a, b) => b.date - a.date)
+            setAllTournaments(tournaments)
+        }
+        fetchData()
+    })
 
     return(
         <>
             <NavigationBar />
-            <h1>Historique</h1>
+            <div id="historyContainer">
+                <h2 id="title">Historique de parties: </h2>
+
+                <Table striped bordered hover variant='dark'>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nom</th>
+                            <th>Date</th>
+                            <th>Modèle de blind</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {allTournaments.map((tournament, index) => (
+                            <tr key={index}>
+                                <td>{tournament.id}</td>
+                                <td>{tournament.name}</td>
+                                <td>{convertTimeStamp(tournament.date)}</td>
+                                <td>{tournament.blindName}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
         </>
     )
 }
