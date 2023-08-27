009d5c3e43e0cf05aee9671b38f24416521a8e35
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-27 18:01:01 +0200

    Add tournament's informations

--- src/react-components/tournamentOther.jsx
+++ src/react-components/tournamentOther.jsx
@@ -2,30 +2,45 @@ import { TournamentPlayers } from "./tournamentPlayers"
 import { RecaveButton } from "./recaveButton"
 import { BlindTabModal } from "./blindTabModal"
 import { Container, Row, Col } from "react-bootstrap"
+import { useSelector } from "react-redux"
+import { convertTimeStamp } from "../utils/date"
 
 export default function Other(props) {
+
+    const t = useSelector((state) => state.tournamentInfo);
+    const tournamentPlayers = useSelector((state) => state.tournamentPlayers);
+
+
     return(
         <>
             <div id="otherContainer">
                 <Container>
                     <Row>
-                        <Col> <TournamentPlayers id={props.id}/> </Col>
                         <Col>
-                            <div id="recave">
-                                <h4>Recave: </h4>
+                            <Row> <TournamentPlayers id={props.id}/> </Row>
+                            <Row>
+                            <div id="buttonGroupContainer">
+                                <div id="blindTabModalComponent"> <BlindTabModal /> </div>
                                 <div id="recaveButton"> <RecaveButton /> </div>
-                            </div> 
+                            </div>
+                            </Row>
                         </Col>
-                    </Row>
-                    <Row>
-                        <BlindTabModal />
+                        <Col>
+                            <h4>Informations:</h4>
+                            <div id="informationsContainer">
+                                <p>
+                                    <span> Nom du tournois: </span> {t.name} <br />
+                                    <span> date: </span> {convertTimeStamp(parseInt(t.date))} <br />
+                                    <span> structure de blind: </span> {t.blindName} <br />
+                                    <span> Tapis initial: </span> {t.initialChip} <br />
+                                    <span> Nombre de joueur: </span> {Object.keys(tournamentPlayers.value).length}
+
+                                </p>
+                            </div>
+                        </Col>
                     </Row>
                 </Container>
             </div>
         </>
     )
 }
-
-
-
-
