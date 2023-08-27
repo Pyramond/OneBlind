fcc60467055f40607f62b02e75adb16ad34b979c
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-27 18:23:00 +0200

    show "pause" when it's the pause

--- src/react-components/blind.jsx
+++ src/react-components/blind.jsx
@@ -5,13 +5,14 @@ import { useEffect, useState, useMemo } from 'react';
 export default function Blind() {
 
     const t = useSelector((state) => state.tournamentSteps);
+    const tournamentTimer = useSelector((state) => state.tournamentTimer)
 
     return(
         <>
             <div id="blindContainer">
                 <div id="blindNumberContainer">
-                    <h1 id="blindNumber">{t.currentStep.sb ? t.currentStep.sb : 0}</h1>
-                    <h1 id="blindNumber">{t.currentStep.sb ? t.currentStep.sb * 2 : 0}</h1>
+                    <h1 id="blindNumber">{t.currentStep.sb ? t.currentStep.sb : tournamentTimer.type} </h1>
+                    <h1 id="blindNumber">{t.currentStep.sb ? t.currentStep.sb * 2 : tournamentTimer.type} </h1>
                 </div>
             </div>
         </>
