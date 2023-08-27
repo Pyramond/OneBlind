7724951abc58d9e74207c8844373ead2adab6c82
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-08-27 17:40:27 +0200

    Add Bootstrap's Grid system

--- src/react-components/tournamentOther.jsx
+++ src/react-components/tournamentOther.jsx
@@ -1,15 +1,31 @@
 import { TournamentPlayers } from "./tournamentPlayers"
 import { RecaveButton } from "./recaveButton"
 import { BlindTabModal } from "./blindTabModal"
+import { Container, Row, Col } from "react-bootstrap"
 
 export default function Other(props) {
     return(
         <>
             <div id="otherContainer">
-                <TournamentPlayers id={props.id}/>
-                <RecaveButton />
-                <BlindTabModal />
+                <Container>
+                    <Row>
+                        <Col> <TournamentPlayers id={props.id}/> </Col>
+                        <Col>
+                            <div id="recave">
+                                <h4>Recave: </h4>
+                                <div id="recaveButton"> <RecaveButton /> </div>
+                            </div> 
+                        </Col>
+                    </Row>
+                    <Row>
+                        <BlindTabModal />
+                    </Row>
+                </Container>
             </div>
         </>
     )
 }
+
+
+
+
