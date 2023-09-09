eace2d77a745d47d0dc0119518d65732cb88981c
Dayamond <83555414+Pyramond@users.noreply.github.com>
2023-09-09 15:56:36 +0200

    Reload components when tournament is created

--- src/react-components/allTournaments.jsx
+++ src/react-components/allTournaments.jsx
@@ -1,16 +1,21 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState, useMemo } from 'react';
 import Tournament from "./tournament"
 import { getAllCurrentTournaments } from "../utils/tournaments"
+import { useSelector } from 'react-redux';
+import { change } from "../redux/slices/reload";
+
 
 export default function AllTournament() {
 
     const [allTournaments, setAllTournaments] = useState([])
+    const t = useSelector((state) => state.reload);
+    const effectDependency = useMemo(() => ({ value: t.value, random: Math.random() }), [t.value]);
 
     useEffect(() => {
         getAllCurrentTournaments().then(tournaments => {
             setAllTournaments(tournaments)
         })
-    }, [])
+    }, [effectDependency])
 
     return(
         <>
