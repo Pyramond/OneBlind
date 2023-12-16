32fd4e25bb3f5e409f34650f9284d6e605df679a
Pyramond <dayamond@protonmail.com>
2023-12-16 23:18:59 +0100

    Update spotify player state component

--- src/react-components/spotify/playerSpotify.jsx
+++ src/react-components/spotify/playerSpotify.jsx
@@ -1,27 +1,38 @@
-import { Button } from "react-bootstrap"
 import { useEffect, useState } from "react"
 
 
-export default function PlayerSpotify(props) {
+export default function PlayerSpotify() {
 
     const token = window.localStorage.getItem("spotify_token")
+    const [musicName, setMusicName] = useState("")
+    const[artist, setArtist] = useState("")
+    const [imageUrl, setImageUrl] = useState("")
 
-    function pause() {
-        fetch("https://api.spotify.com/v1/me/player", {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`
-            },
-        })
-        .then(res => res.json())
-        .then(res => {
-            console.warn(res)
-        })
-    }
+    useEffect(() => {
+        const fetchData = () => {
+            fetch("https://api.spotify.com/v1/me/player", {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                },
+            })
+            .then(res => res.json())
+            .then(res => {
+                setMusicName(res.item.name)
+                setArtist(res.item.artists[0].name)
+                setImageUrl(res.item.album.images[2].url)
+            })
+        }
+        const intervalId = setInterval(fetchData, 3000)
+        return () => clearInterval(intervalId)
+    }, [])
 
     return (
         <>
-            {token ? <Button onClick={pause}>Pause</Button> : <h1>!Token</h1>}
+            <div id="spotifyPlayer">
+                <img src={imageUrl} alt={`${musicName} - ${artist} Image`} />
+                <p>{musicName} <br /> {artist}</p>
+            </div>
         </>
     )
 }
