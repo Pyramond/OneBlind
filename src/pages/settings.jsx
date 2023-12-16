7fbfc44406a46b950414439814ea4489f2ad69b6
Pyramond <dayamond@protonmail.com>
2023-12-16 14:47:21 +0100

    add settings page

---
+++ src/pages/settings.jsx
@@ -0,0 +1,13 @@
+import NavigationBar from '../react-components/navbar'
+import LoginSpotify from '../react-components/spotify/loginSpotify'
+
+export default function Settings() {
+
+  return (
+    <>
+        <NavigationBar />
+        <LoginSpotify />
+    
+    </>
+  )
+}
