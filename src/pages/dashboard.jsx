b741a3589d970d74b65a9497a06767bcaf25537e
Pyramond <dayamond@protonmail.com>
2024-01-21 18:21:51 +0100

    remove components

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,17 +1,9 @@
-import CreateTournament from '../react-components/createTournament'
-import CreateBlindModel from '../react-components/createBlindModel'
-import AllTournament from '../react-components/allTournaments'
-
 export default function Dashboard() {
 
   return (
     <>
       <div id="homeContainer">
-        <CreateTournament />
-        <CreateBlindModel />
       </div>
-
-      <AllTournament />
     </>
   )
 }
