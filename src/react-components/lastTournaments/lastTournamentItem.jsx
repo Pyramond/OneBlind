89b3b9c336a58f00da178acb445baa4c0f781cc8
Pyramond <dayamond@protonmail.com>
2024-04-24 19:32:45 +0200

    Add hoverCard when the name is too long

--- src/react-components/lastTournaments/lastTournamentItem.jsx
+++ src/react-components/lastTournaments/lastTournamentItem.jsx
@@ -1,7 +1,17 @@
+import { HoverCard } from "@mantine/core"
+
+
 export function LastTournamentItem(props) {
     return(
         <div id="lastTournamentItem">
-            <h1> {props.name.length >= 13 ? props.name.substring(0, 13) + "..." : props.name} </h1>
+            <HoverCard>
+                <HoverCard.Target>
+                    <h1> {props.name.length >= 13 ? props.name.substring(0, 13) + "..." : props.name} </h1>
+                </HoverCard.Target>
+                <HoverCard.Dropdown>
+                    {props.name}
+                </HoverCard.Dropdown>
+            </HoverCard>
             <h1> {props.date} </h1>
         </div>
     )
