fb9878d9122786a50cd77f079bb26a627b4cb178
Pyramond <pyramond69@gmail.com>
2025-05-01 01:10:52 +0200

    fix: responsive items

--- src/pages/tournament/tournamentPage.jsx
+++ src/pages/tournament/tournamentPage.jsx
@@ -10,37 +10,58 @@ import MyTimer from '../../react-components/timer';
 import Blind from '../../react-components/blind';
 import Other from '../../react-components/tournamentOther';
 import AvStack from '../../react-components/avStack';
-import { SimpleGrid } from '@mantine/core';
+import {Flex, SimpleGrid} from '@mantine/core';
 
 export default function TournamentPage() {
-
     const { id } = useParams();
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     useEffect(() => {
-      async function fetchData() {
-        const players = await getTournamentPlayers(id);
-        dispatch(setPlayers(players));
+        async function fetchData() {
+            const players = await getTournamentPlayers(id);
+            dispatch(setPlayers(players));
 
-        const infos = await getTournamentById(id);
-        infos.number = players.length;
-        dispatch(setInfos(infos));
+            const infos = await getTournamentById(id);
+            infos.number = players.length;
+            dispatch(setInfos(infos));
 
-        const steps = await getModelById(infos.blindId);
-        dispatch(setSteps(steps.steps));
-        dispatch(changeStep());
-      }
-      fetchData()
+            const steps = await getModelById(infos.blindId);
+            dispatch(setSteps(steps.steps));
+            dispatch(changeStep());
+        }
+        fetchData();
     }, []);
 
-  return (
-    <>
-      <SimpleGrid cols={2}>
-          <Blind />
-          <MyTimer />
-          <Other id={id} />
-          <AvStack />
-      </SimpleGrid>
-    </>
-  )
+    return (
+        <Flex
+            direction="column"
+            h="100vh"
+            gap="8em"
+            p="1em"
+        >
+            <Flex style={{ flex: 0.35 }}>
+                <SimpleGrid
+                    cols={2}
+                    breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
+                    spacing="xl"
+                    w="100%"
+                >
+                    <Blind />
+                    <MyTimer />
+                </SimpleGrid>
+            </Flex>
+
+            <Flex style={{ flex: 0.5 }}>
+                <SimpleGrid
+                    cols={2}
+                    breakpoints={[{ maxWidth: 'sm', cols: 1 }]}
+                    spacing="xl"
+                    w="100%"
+                >
+                    <Other id={id} />
+                    <AvStack />
+                </SimpleGrid>
+            </Flex>
+        </Flex>
+    );
 }
