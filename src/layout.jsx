38d73c0be2b48e4b678f4eca8aacfb184c8ad56a
Pyramond <pyramond69@gmail.com>
2025-05-01 02:23:03 +0200

    style: Change title logo

--- src/layout.jsx
+++ src/layout.jsx
@@ -31,8 +31,8 @@ export function Layout() {
             <Link to="/">
                 <Image
                     radius="md"
-                    h={50}
-                    src="/images/OneBlind.svg"
+                    h={65}
+                    src="/images/one-blinde.png"
                 />
             </Link>
 
